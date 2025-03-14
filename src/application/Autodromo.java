2e26cacb124252c9a77cb27e7a5df4aabebca312
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-03-14 17:38:02 -0300

    02/03

---
+++ src/application/Autodromo.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.Carro;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro audi = new Carro();
+
+    }
+}
--- src/aulas/dio/poo/Student.java
+++
@@ -1,7 +0,0 @@
-package aulas.dio.poo;
-
-public class Student {
-    public static void main(String[] args) {
-
-    }
-}
